a3d757262bbde03ff1a014e40340940a44a38fb1
Mark Mathew <markmathew@Marks-MacBook-Pro-2.local>
2024-02-16 14:27:53 -0800

    add signin page to route. Allows user to only see facial detection after successful signin

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ParticlesBg from 'particles-bg';
 import Clarifai from 'clarifai';
 // import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
+import Signin from './components/Signin/Signin';
 
 
 // const app = new Clarifai.App({
@@ -91,21 +92,78 @@ import './App.css';
 
 //     })
 //     .catch(error => console.log('error', error));
+// onInputChange = (event) => {
+//     this.setState({input: event.target.value});
+// }
 
 
-class App extends React.Component {
+
+
+
+// onButtonSubmit = () => {
+//     this.setState({imageUrl: this.state.input});
+//     app.models
+//     .predict(
+//         Clarifai.FACE_DETECT_MODEL,
+//         this.state.input)
+//     .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+//     .catch(err => console.log(err))
+// }
+
+class App extends Component {
+    constructor()
+    {
+        super();
+        this.state = {
+            input: '',
+            imageUrl: '',
+            box: {},
+            route: 'signin'
+        }
+    }
+
+    calculateFaceLocation = (data) => {
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.getElementById('inputimage');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        return {
+          leftCol: clarifaiFace.left_col * width,
+          topRow: clarifaiFace.top_row * height,
+          rightCol: width - (clarifaiFace.right_col * width),
+          bottomRow: height - (clarifaiFace.bottom_row * height)
+        }
+      }
+    
+      displayFaceBox = (box) => {
+        this.setState({box: box});
+      }
+      
+      onRouteChange = (route) => {
+        // must wrap objects in {}
+        this.setState({route: 'home'});
+      }
+
   render() {
     return (
       <div className="App">
-         <ParticlesBg color="#ffffff" num={100} type="cobweb" bg={true} />
-        <Navigation/>
-        <Rank/>
-        <ImageLinkForm />
-        <Logo/>
-        {/* <FaceRecognition/> */}
+         <ParticlesBg className='particles'
+         color="#ffffff" num={100} type="cobweb" bg={true} />
+        <Navigation onRouteChange={this.onRouteChange} />
+        { this.state.route === 'signin' 
+        // if successful signin, will redirect to the home page
+        ? <Signin onRouteChange={this.onRouteChange} />
+        : <div>
+            <Logo />
+            <Rank/>
+            <ImageLinkForm 
+            onInputChange={this.onInputChange}
+            onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition box = {this.state.box} imageUrl={this.state.imageUrl} />
       </div>
-    );
   }
+  </div>
+  )}
 }
 
 export default App;
